29295003b9bfe0ece48940578063116cd7c411b9
Christian Dean <christiandeandemesa@gmail.com>
2022-10-21 21:51:47 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import {Routes, Route} from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+	return (
+		<div className='backgroundContainer'>
+			<Routes>
+				<Route />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
